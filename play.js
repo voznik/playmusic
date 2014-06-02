932b728f08e1c1211da6e1cd5b3559746704c438
Jamon Terrell <git@jamonterrell.com>
2014-06-01 21:15:41 -0500

    cleanup... still working on fixing util.request into this.request and abstracting more repetative code.

--- play.js
+++ play.js
@@ -3,7 +3,7 @@
  * Based on the work of the Google Play Music resolver for Tomahawk
  * and the gmusicapi project by Simon Weber.
  */
-
+var fs = require('fs');
 var https = require('https');
 var querystring = require('querystring');
 var url = require('url');
@@ -48,17 +48,65 @@ util.parseKeyValues = function(body) {
     });
     return obj;
 };
+util.Base64 = {
+    _map: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_",
+    stringify: CryptoJS.enc.Base64.stringify,
+    parse: CryptoJS.enc.Base64.parse
+};
+util.salt = function(len) {
+    return Array.apply(0, Array(len)).map(function() {
+        return (function(charset){
+            return charset.charAt(Math.floor(Math.random() * charset.length));
+        }('abcdefghijklmnopqrstuvwxyz0123456789'));
+    }).join('');
+};
+
 
 var PlayMusic = function() {};
 
 PlayMusic.prototype._baseURL = 'https://www.googleapis.com/sj/v1/';
 PlayMusic.prototype._webURL = 'https://play.google.com/music/';
+PlayMusic.prototype._mobileURL = 'https://android.clients.google.com/music/';
+PlayMusic.prototype._accountURL = 'https://www.google.com/accounts/';
 PlayMusic.prototype.cacheTime = 300;
+
+
+PlayMusic.prototype.request = function(reqUrl, options, data, success, error) {
+    var opt = url.parse(reqUrl);
+    console.log("\nreqUrl\t", reqUrl, "\noptions\t", options, "\ndata\t", data);
+    Object.keys(options).forEach(function(k) {
+        opt[k] = options[k];
+    });
+    console.log("opt", opt);
+    var req = https.request(opt, function(res) {
+        console.log("result!", res.statusCode, res.headers);
+        res.setEncoding('utf8');
+        var body = "";
+        res.on('data', function(chunk) {
+            body += chunk;
+        });
+        res.on('end', function() {
+            if(res.statusCode === 200) {
+                success(body, res);
+            } else {
+                error(body, null, res);
+            }
+        });
+        res.on('error', function() {
+            error(null, Array.prototype.slice.apply(arguments), res);
+        });
+    });
+    if(typeof data !== "undefined" && data !== null) req.write(data);
+    req.end();
+};
+
 
 PlayMusic.prototype.init = function(callback) {
     var that = this;
-    this._email = "";  // @TODO load this from something
-    this._password = ""; // @TODO load this from something
+    var config = JSON.parse(fs.readFileSync("config.json"));
+
+    this._email = config.email;
+    this._password = config.password;
 
     // load signing key
     var s1 = CryptoJS.enc.Base64.parse('VzeC4H4h+T2f0VI180nVX8x+Mb5HiTtGnKgH52Otj8ZCGDz9jRWyHb6QXK0JskSiOgzQfwTY5xgLLSdUSreaLVMsVVWfxfa8Rw==');
@@ -73,10 +121,9 @@ PlayMusic.prototype.init = function(callback) {
     this._login(function() {
         that._loadWebToken(function() {
             that._loadSettings(function() {
-                that._getData(function (response) {
-                    that.trackCount = response.data.items.length;
+                //that._getData(function (response) {
                     callback();
-                });
+                //});
                 that._ready = true;
             });
         });
@@ -97,7 +144,6 @@ PlayMusic.prototype._getData = function(callback) {
                     response: JSON.parse(data),
                     time: Date.now()
                 };
-                console.log(data);
                 callback(that.cachedRequest.response);
             },
             function(data, err, res) {
@@ -106,45 +152,10 @@ PlayMusic.prototype._getData = function(callback) {
         );
     }
 };
-// @TODO fix this
-PlayMusic.prototype._execSearch = function(query, callback, max_results) {
-    var that = this;
-    this._getData(function (response) {
-        var results = { tracks: [], albums: [], artists: [] };
-        for (var idx = 0; idx < response.data.items.length; idx++) {
-            var entry = response.data.items[ idx ];
-            var lowerQuery = query.toLowerCase();
-            if (entry.artist.toLowerCase() === lowerQuery || entry.album.toLowerCase() === lowerQuery || entry.title.toLowerCase() === lowerQuery) {
-                var artist = that._convertArtist(entry);
-                var album = that._convertAlbum(entry);
-                if (!that.containsObject(artist, results.artists)) {
-                    results.artists.push(artist);
-                }
-                if (!that.containsObject(album, results.albums)) {
-                    results.albums.push(album);
-                }
-                results.tracks.push(that._convertTrack(entry));
-            }
-        }
-        callback.call( window, results );
-    });
 
-};
-
-PlayMusic.prototype.getStreamUrl = function (id) {
-    // TODO - this is required for the All Access part of Google Music
-    // generate 13-digit numeric salt
-    var salt = '' + Math.floor( Math.random() * 10000000000000 );
-
-    // generate SHA1 HMAC of track ID + salt
-    // encoded with URL-safe base64
-    var sig = CryptoJS.HmacSHA1( id + salt, this._key )
-            .toString( CryptoJS.enc.Base64 )
-            .replace( /\=+$/, '' )   // no padding
-            .replace( /\+/g, '-' )  // URL-safe alphabet
-            .replace( /\//g, '_' )  // URL-safe alphabet
-        ;
-
+PlayMusic.prototype.getStreamUrl = function (id, callback) {
+    var salt = util.salt(13);
+    var sig = CryptoJS.HmacSHA1(id + salt, this._key).toString(util.Base64);
     var qp = {
         u: "0",
         net: "wifi",
@@ -161,14 +172,18 @@ PlayMusic.prototype.getStreamUrl = function (id) {
 
     var qstring = querystring.stringify(qp);
     util.request(
-        'https://android.clients.google.com/music/mplay?' + qstring, 
+        this._mobileURL + 'mplay?' + qstring, 
         {method: "GET", headers: { "Content-type": "application/x-www-form-urlencoded", "Authorization": "GoogleLogin auth=" + this._token, "X-Device-ID": this._deviceId} },
         null,
         function(data, res) {
             console.log(data);
         },
         function(data, err, res) {
-            console.log("error getting stream urls", res.statusCode, data, err);
+            if(res.statusCode === 302) {
+                callback(res.headers.location);
+            } else {
+                console.log("error getting stream urls", res.statusCode, data, err);
+            }
         }
     );
 };
@@ -177,32 +192,24 @@ PlayMusic.prototype._loadSettings = function (callback) {
     var that = this;
 
     util.request(
-        this._webURL + "services/loadsettings?u=0&xt=" + encodeURIComponent(this._xt),
+        this._webURL + "services/loadsettings?" + querystring.stringify({u: 0, xt: this._xt}),
         { method: "POST", headers: {  "Authorization": "GoogleLogin auth=" + this._token, "Content-Type": "application/json" } },
         JSON.stringify({"sessionId": ""}),
         function(body, res) {
             var response = JSON.parse(body);
-            console.log("response", response);
             that._allAccess = response.settings.isSubscription;
             console.log("Google Play Music All Access is ", (that._allAccess ? "enabled" : "disabled" ));
 
-            var device = null;
-            var devices = response.settings.devices;
-            for (var i = 0; i < devices.length; i++) {
-                var entry = devices[i];
-                if ('PHONE' == entry.type) {
-                    device = entry;
-                    break;
-                }
-            }
-
-            if(device) {
-                that._deviceId = device.id.slice(2);
+            var devices = response.settings.devices.filter(function(d) {
+                return d.type === "PHONE";
+            });
+            if(devices.length > 0) {
+                that._deviceId = devices[0].id.slice(2);
                 console.log("DEVICE ID", that._deviceId);
-
-                console.log("using device ID from " + device.carrier + " " + device.manufacturer + " " + device.model);
+                console.log("using device ID from " + devices[0].carrier + " " + devices[0].manufacturer + " " + devices[0].model);
 
                 callback();
+
             } else {
                 console.log("unable to find a device on your account");
             }
@@ -256,16 +263,6 @@ PlayMusic.prototype._login =  function (callback) {
     var that = this;
     this._token = null;
 
-    // if a login is already in progress just queue the callback
-    if (this._loginLock) {
-        this._loginCallbacks.push( callback );
-        return;
-    }
-
-    this._loginLock = true;
-    this._loginCallbacks = [ callback ];
-
-
     var data = {
         accountType: "HOSTED_OR_GOOGLE",
         Email: that._email.trim(),
@@ -273,18 +270,14 @@ PlayMusic.prototype._login =  function (callback) {
         service: "sj",
         source: "node-gmusic"
     };
-    util.request("https://www.google.com/accounts/ClientLogin",
+    util.request(this._accountURL + "ClientLogin",
         { method: "POST", headers: { "Content-type": "application/x-www-form-urlencoded" }},
         querystring.stringify(data),
         function(data, res) {
             var obj = util.parseKeyValues(data);
             console.log("login success!", obj);
             that._token = obj.Auth;
-            that._loginLock = false;
-            for (var idx = 0; idx < that._loginCallbacks.length; idx++) {
-                that._loginCallbacks[idx]();
-            }
-            that._loginCallbacks = null;
+            callback();
         },
         function(data, err, res) {
             console.log("login failed!", res.statusCode, data, err);
@@ -299,5 +292,5 @@ pm.init(function() {
 //    pm.getStreamUrl("84df1e4e-6b76-3147-9a78-a44becc28dc5");
     //pm.getStreamUrl("6f7120c8-4454-316e-a3ea-df042887fe00");
     //pm.getStreamUrl("d895bf67-c3d8-3b4a-b7e8-cc5df5c4e0b2");
-    pm.getStreamUrl("7f405ca5-4b9b-378a-a0db-72764a215cda");
+    pm.getStreamUrl("0dfc52a6-a9e1-3471-845c-f99eaa832ce2", console.log);
 });
