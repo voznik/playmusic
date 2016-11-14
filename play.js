6c0716d580d5dd2e5b57727bd8ea742cfda08e88
Adam Duncan <adam.jd@gmail.com>
2016-11-14 17:07:21 +0000

    Return all result types in search. Fixes #52

--- play.js
+++ play.js
@@ -336,6 +336,7 @@ PlayMusic.prototype.search = function (text, maxResults, callback) {
     var that = this;
     var qp = {
         q: text,
+        ct: '1,2,3,4,5,6,7,8,9',
         "max-results": maxResults
     };
     var qstring = querystring.stringify(qp);
